4c02e75cb405feeff1a22ec817cc93ec5d91247e
KiranShaw-QI <kiran.shaw@quantuminventions.com>
2022-11-04 14:52:17 +0530

    1. Changed package name to  com.quantuminventions.customkeyboard

---
+++ app/src/main/java/com/quantuminventions/customkeyboard/app/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.quantuminventions.customkeyboard.app
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.quantuminventions.customkeyboard.components.keyboard.CustomisedKeyboardView
+import com.quantuminventions.customkeyboard.components.keyboard.CustomKeyboardView
+
+
+/**
+ * Created by Don.Brody on 7/18/18.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var keyboard: CustomisedKeyboardView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberField: EditText = findViewById(R.id.testNumberField)
+        val numberDecimalField: EditText = findViewById(R.id.testNumberDecimalField)
+        val qwertyField: EditText = findViewById(R.id.testQwertyField)
+
+        keyboard = findViewById(R.id.customKeyboardView)
+        keyboard.setTextFontSize(30.0F)
+        keyboard.setDecimalsSeparator(',')
+        keyboard.setThousandsSeparator('.')
+        keyboard.setBackgroundColor(Color.WHITE)
+        keyboard.registerEditText(CustomKeyboardView.KeyboardType.NUMBER, numberField)
+        keyboard.registerEditText(
+            CustomKeyboardView.KeyboardType.NUMBER_DECIMAL,
+            numberDecimalField
+        )
+        // can separately add any TextWatcher
+        numberDecimalField.addTextChangedListener(
+            CustomTextWatcher(
+                numberDecimalField,
+                ',',
+                '.'
+            )
+        )
+        keyboard.registerEditText(CustomKeyboardView.KeyboardType.QWERTY, qwertyField)
+
+        val switchActivitiesButton: Button = findViewById(R.id.switchActivitiesButton)
+        switchActivitiesButton.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    AdvancedFeaturesActivity::class.java
+                )
+            )
+        }
+    }
+
+    override fun onBackPressed() {
+        if (keyboard.isExpanded) {
+            keyboard.translateLayout()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
---
+++ customdecimalkeyboard/src/main/java/com/quantuminventions/customdecimalkeyboard/components/keyboard/CustomisedKeyboardView.kt
@@ -0,0 +1,36 @@
+package com.quantuminventions.customkeyboard.components.keyboard
+
+import android.content.Context
+import android.util.AttributeSet
+import com.quantuminventions.customkeyboard.components.keyboard.CustomKeyboardView
+
+/**
+ * tracksynq-android
+ *
+ * Created by Kiran Shaw on 21-01-2022
+ * Copyright © 2022 Quantum Inventions. All rights reserved.
+ */
+
+class CustomisedKeyboardView(context: Context, attr: AttributeSet) :
+    CustomKeyboardView(context, attr) {
+
+    fun setDecimalsSeparator(decSeparator: Char) {
+        decimalSeparator = decSeparator
+    }
+
+    fun setThousandsSeparator(thousandsSeparator: Char) {
+        thousandSeparator = thousandsSeparator
+    }
+
+    fun setTextFontSize(txtSize: Float) {
+        textSize = txtSize
+    }
+
+    fun setBtnGapSize(gapSize: Int) { // in Dp
+        this.gapSize = gapSize
+    }
+
+    override fun setBackgroundColor(bgColor: Int) {
+        this.bgColor = bgColor
+    }
+}
