03679dad34c44e7f1f69a1755332e64646d01463
kiran.shaw_QI <kiran.shaw@quantuminventions.com>
2023-01-07 05:09:17 +0530

    Task/redmine#98080 publish in GitHub packages (#2)
    * 1. Changed package name to  com.quantuminventions.customkeyboard
    
    * 1. Implemented changes for publishing the library - customdecimalkeyboard module to github packages.
    
    * 1. Pushed some changes in build.gradle files.
    
    * 1. Pushed some drawables files
    
    * 1. Refactored some changes
    
    * 1. Refactored some changes
    
    * 1. Refactored some changes
    
    * fix: upgrade gradle to 7.2
    
    * 1. Library version bumped with @iad github credentials
    
    Co-authored-by: Akhan Zhakiyanov <akhan.zhakiyanov@quantuminventions.com>

--- customdecimalkeyboard/src/main/java/com/kiranshaw/customdecimalkeyboard/components/expandableView/ExpandableStateListener.kt
+++
@@ -1,8 +0,0 @@
-package com.kiranshaw.customdecimalkeyboard.components.expandableView
-
-/**
- * Created by Don.Brody on 7/21/18.
- */
-interface ExpandableStateListener {
-    fun onStateChange(state: ExpandableState)
-}
